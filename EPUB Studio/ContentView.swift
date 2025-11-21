6e9521b5e222cab65c791773b96f2d7f5cb33ef7
MShin1 <karasawa429@gmail.com>
2025-11-21 20:28:37 +0900

    Initial Commit

---
+++ EPUB Studio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EPUB Studio
+//
+//  Created by 望月慎一 on 2025/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EPUB StudioTests/EPUB_StudioTests.swift
@@ -0,0 +1,17 @@
+//
+//  EPUB_StudioTests.swift
+//  EPUB StudioTests
+//
+//  Created by 望月慎一 on 2025/11/21.
+//
+
+import Testing
+@testable import EPUB_Studio
+
+struct EPUB_StudioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
